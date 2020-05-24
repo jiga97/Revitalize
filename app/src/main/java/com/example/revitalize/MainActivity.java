6434f80795b80c9a3bd76cbdd614914025f67de5
Snowflakeblaze <somapritesh@gmail.com>
2020-05-24 14:14:12 +0200

    commit_1

--- app/src/main/java/com/example/revitalize/MainActivity.java
+++ app/src/main/java/com/example/revitalize/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.revitalize;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,11 +11,19 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ///mainscreen toolbar
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Community");
+
+/*
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
@@ -24,5 +33,8 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
         }
+
+
+ */
     }
 }
